1166f7013f2d221578b85163efc88d14e10b600a
+ <+>
2022-12-08 12:02:41 +0300

    add validations and attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ has_secure_password
 
 validates :username, presence:true, uniqueness:true
 validates :username, length: { mininum:5}
-#validates :password_digest, presence: true
 
 has_many :workouts
 has_many :exercises

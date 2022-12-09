ac178e054abc3188e9ad1b214e6e742301d6750b
+ <+>
2022-12-09 15:22:23 +0300

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :exercises
-  resources :workouts
-  resources :users
+  resources :exercises, only: [:create, :update, :destroy]
+  resources :users, only: [:create, :show]
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

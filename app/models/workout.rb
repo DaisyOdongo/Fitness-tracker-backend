3d7782d37dfafcba1df1f7fe505b0e4726d77d7d
+ <+>
2022-12-09 15:21:41 +0300

    delete workout table

--- app/models/workout.rb
+++
@@ -1,7 +0,0 @@
-class Workout < ApplicationRecord
-  validates :type, presence: true
-  validates :description, length: { minimum: 50 }
-  validates :duration, length: { minimum: 60 }
-    belongs_to :user
-
-end

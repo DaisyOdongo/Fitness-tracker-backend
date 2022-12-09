4b38ae37be84e3d705460b2187c299316cc4d157
+ <+>
2022-12-09 21:57:04 +0300

    setting migrations

--- db/schema.rb
+++ db/schema.rb
@@ -27,13 +27,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_080635) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "workouts", force: :cascade do |t|
-    t.string "type"
-    t.string "description"
-    t.time "duration"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-end
+  

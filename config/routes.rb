26091905d530332da8fb87d43e5b219dbfced2f9
+ <+>
2022-12-09 21:56:29 +0300

    some nimor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :exercises, only: [:create, :update, :destroy]
   resources :users, only: [:create, :show]
+  #get "/user", to: "home#index"
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
@@ -8,5 +9,4 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
--- db/migrate/20221208080635_create_exercises.rb
+++ db/migrate/20221208080635_create_exercises.rb
@@ -2,9 +2,11 @@ class CreateExercises < ActiveRecord::Migration[7.0]
   def change
     create_table :exercises do |t|
       t.string :name
+      t.string :type
       t.integer :sets
       t.integer :weight
-      t.integer :workout_id
+      t.integer :duration
+      t.integer :user_id
 
       t.timestamps
     end
